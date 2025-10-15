9d2647d6fa5f88ac0ae7861ad08f8de2c399a02b
JardsonFlorentino <jardsonflorentino@gmail.com>
2025-10-15 18:07:02 -0300

    app.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
-import app from './app'
+import app from './app.js'
 
-app.listen(3001, () => console.log('Server is running at port 3001...'))
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
