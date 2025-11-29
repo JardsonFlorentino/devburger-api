c6ea3df9ebcf468d82ec100a9d501e992ce1ac80
JardsonFlorentino <jardsonflorentino@gmail.com>
2025-11-28 21:33:13 -0300

    database.js

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -10,4 +10,10 @@ module.exports = {
     underscored: true,
     underscoredAll: true,
   },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 }
