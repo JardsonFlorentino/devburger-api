e62c306a2cb8c07270c403679c58fa83885404eb
JardsonFlorentino <jardsonflorentino@gmail.com>
2026-02-05 20:48:11 -0300

    mudanças para o Railway

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,10 +1,6 @@
 module.exports = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  use_env_variable: 'DATABASE_URL',
+  dialect: 'postgres',
   define: {
     timestamps: true,
     underscored: true,
